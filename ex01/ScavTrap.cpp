ca6db083f8acf33c1e90ca5592fd65b277c792d8
Serhii Veretennikov <gridis72@gmail.com>
2025-05-04 13:23:30 +0200

    feat: implmented exercise

---
+++ ex01/ScavTrap.cpp
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svereten <svereten@student.42vienna.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/03 17:58:08 by svereten          #+#    #+#             */
+/*   Updated: 2025/05/04 13:21:07 by svereten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ScavTrap.hpp"
+#include <iostream>
+
+ScavTrap::ScavTrap(): ClapTrap("John", 100, 50, 20)
+{
+	std::cout << "ScavTrap " << getName() << " ready!\n";
+}
+
+ScavTrap::ScavTrap(const std::string &name): ClapTrap(name, 100, 50, 20)
+{
+	std::cout << "ScavTrap " << getName() << " ready!\n";
+}
+
+ScavTrap::ScavTrap(const ScavTrap &scavTrap): ClapTrap(scavTrap)
+{
+	std::cout << "ScavTrap " << getName() << " ready!\n";
+}
+
+ScavTrap::~ScavTrap() {
+	std::cout << "ScavTrap " << getName() << " shutting down :(\n";
+}
+
+ScavTrap	&ScavTrap::operator=(const ScavTrap &scavTrap) {
+	if (this != &scavTrap) {
+		setName(scavTrap.getName());
+		setHp(scavTrap.getHp());
+		setMaxHp(scavTrap.getMaxHp());
+		setEp(scavTrap.getEp());
+		setAttack(scavTrap.getAttack());
+	}
+	return (*this);
+}
+
+void	ScavTrap::attack(const std::string &target) {
+		if (!getHp()) {
+		std::cout << "ScavTrap " << getName()
+			<< " is dead and can't attack\n";
+		return ;
+	}
+	if (!getEp()) {
+		std::cout << "ScavTrap " << getName()
+			<< " doesn't have enough energy for the attack\n";
+		return ;
+	}
+	std::cout << "ScavTrap " << getName()
+		<< " attacks " << target
+		<< ", causing " << getAttack()
+		<< " points of damage!\n";
+	setEp(getEp() - 1);
+	
+}
+
+void	ScavTrap::guardGate() {
+	std::cout << "ScavTrap " << getName()
+		<< ": \"You shall not pass!\"\n";
+}
---
+++ ex01/main.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svereten <svereten@student.42vienna.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/03 15:47:50 by svereten          #+#    #+#             */
+/*   Updated: 2025/05/04 13:21:59 by svereten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ScavTrap.hpp"
+
+int	main(void) {
+	ScavTrap	trap("Johnny");
+	ScavTrap	john;
+	ScavTrap	jane = ScavTrap("Jane");
+
+	jane = john;
+
+
+	trap.attack("1");
+	trap.attack("2");
+	trap.attack("3");
+	trap.attack("4");
+	trap.attack("5");
+	trap.attack("6");
+	trap.attack("7");
+
+	trap.takeDamage(5);
+	trap.beRepaired(2);
+	trap.beRepaired(10);
+
+	trap.attack("8");
+	trap.attack("9");
+	trap.attack("10");
+	trap.attack("11");
+	trap.beRepaired(10);
+	trap.takeDamage(10);
+
+	trap.attack("12");
+	trap.beRepaired(10);
+	trap.takeDamage(10);
+
+	trap.guardGate();
+	trap.guardGate();
+
+}
